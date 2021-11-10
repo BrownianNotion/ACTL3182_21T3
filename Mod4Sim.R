071cdeaa18d12a1cdedbac213d23f97da9f4320e
BrownianNotion <z5163239@unsw.edu.au>
2021-11-11 02:13:35 +1100

    Added aesthetic changes to gbm simulation

--- Mod4Sim.R
+++ Mod4Sim.R
@@ -1,22 +1,29 @@
 library(ggplot2)
 library(gganimate)
 library(gifski)
+library(reshape2)
+set.seed(0)
+
 #Brownian Motion Simulation
 Term = 1; N = 500
-mu = 0.05; sigma = 0.05
+mu = 0.05; sigma = 0.01
 dt = Term/N
 dW = rnorm(N, mean = 0, sd = sqrt(dt))
 W = c(0, cumsum(dW))
-Time = seq(from = 0, to = 1, by = dt)
-S = exp((mu - 0.5*sigma^2)*Time + sigma*W)
+t = seq(from = 0, to = 1, by = dt)
+S = exp((mu - 0.5*sigma^2)*t + sigma*W)
+
 
 #Plotting
-df <- data.frame(Time, W, S)
-p <- ggplot(df) +
-  geom_line(aes(x = Time, y = S), colour = "black") +
-  ggtitle("Geometric Brownian Motion")
-  #geom_line(aes(x = Time, y = W), colour = "red")
+S_mean <- exp(mu*t)
+df <- data.frame(t, S, S_mean)
+colnames(df) <- c("t", "GBM", "Mean")
+df_melted <- melt(df, id.vars="t", variable.name="Process")
+
+p <- ggplot(df_melted) +
+  geom_line(aes(x=t, y=value, group=Process, colour=Process), lwd=1) +
+  ggtitle("Geometric Brownian Motion vs Mean return")
 
 #Animate plot.
-p + transition_reveal(Time)
+p + theme(legend.position="right") + transition_reveal(t)
 
